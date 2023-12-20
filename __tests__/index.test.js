c9e8568162913abc657e2c9bcadc730bc0220ff6
nana park <obsidinan@gmail.com>
2023-12-20 12:50:38 +0300

    added test yml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,10 +8,20 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+let result;
+beforeEach(() => {
+  result = readFileSync(getFixturePath('result.txt'), 'utf8');
+});
+
 test('json', () => {
   const filename1 = getFixturePath('file1.json');
   const filename2 = getFixturePath('file2.json');
-  const result = readFileSync(getFixturePath('result.txt'), 'utf8');
+  expect(getDiff(filename1, filename2)).toEqual(result);
+});
+
+test('yaml', () => {
+  const filename1 = getFixturePath('file1.yml');
+  const filename2 = getFixturePath('file2.yml');
 
   expect(getDiff(filename1, filename2)).toEqual(result);
 });
