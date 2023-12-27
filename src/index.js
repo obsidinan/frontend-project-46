c8c9706808bf10dec8f47282fe7d59aabbe6c0f3
nana park <obsidinan@gmail.com>
2023-12-27 23:45:20 +0300

    fix index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import path from 'path';
 import { readFileSync } from 'node:fs';
 import parse from './parsers.js';
 import compare from './compare.js';
-import formating from './formatters/index.js';
+import formatting from './formatters/index.js';
 
 const getExt = (filename) => path.extname(filename).slice(1);
 
@@ -16,7 +16,7 @@ const getDiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = getData(filepath1);
   const data2 = getData(filepath2);
 
-  return formating(compare(data1, data2), format);
+  return formatting(compare(data1, data2), format);
 };
 
 const genDiff = (file1, file2, format = 'stylish') => {
