7d158890d9e0868bcf4ad06dafc49c7244cce218
nana park <obsidinan@gmail.com>
2023-12-28 01:40:03 +0300

    fix bugs

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,7 +8,9 @@ const prepareValue = (value) => {
 };
 
 const formatPlain = (data, path = []) => {
-  const result = data.map((item) => {
+  const result = data
+    .filter((item) => item.type !== 'unchanged')
+    .map((item) => {
       const currentPath = path.concat(item.key);
       const fullPath = currentPath.join('.');
       switch (item.type) {
@@ -25,13 +27,11 @@ const formatPlain = (data, path = []) => {
         }
         case 'nested':
           return formatPlain(item.children, currentPath);
-        case 'unchanged':
-          return '';
         default:
           throw new Error('Unknown type!');
       }
     });
-  return result.filter((outputLine) => outputLine.length > 0).join('\n');
+  return result.join('\n');
 };
 
 export default formatPlain;
