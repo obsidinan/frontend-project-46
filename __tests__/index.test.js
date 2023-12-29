ee4e5b45a1c24ea56491448ce02a193a82e58a3e
nana park <obsidinan@gmail.com>
2023-12-30 00:46:32 +0300

    added tests for json and plain

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,16 +8,30 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const result = readFileSync(getFixturePath('result.txt'), 'utf8');
+const resultStylish = readFileSync(getFixturePath('result-stylish.txt'), 'utf8');
+const resultPlain = readFileSync(getFixturePath('result-plain.txt'), 'utf8');
+const resultJSON = readFileSync(getFixturePath('result-json.txt'), 'utf8');
 
-test('json', () => {
+test('stylish files json', () => {
   const filename1 = getFixturePath('file1.json');
   const filename2 = getFixturePath('file2.json');
-  expect(getDiff(filename1, filename2)).toEqual(result);
+  expect(getDiff(filename1, filename2)).toEqual(resultStylish);
 });
 
-test('yaml', () => {
+test('stylish files yaml', () => {
   const filename1 = getFixturePath('file1.yml');
   const filename2 = getFixturePath('file2.yml');
-  expect(getDiff(filename1, filename2)).toEqual(result);
+  expect(getDiff(filename1, filename2)).toEqual(resultStylish);
+});
+
+test('plain output', () => {
+  const filename1 = getFixturePath('file1.json');
+  const filename2 = getFixturePath('file2.json');
+  expect(getDiff(filename1, filename2, 'plain')).toEqual(resultPlain);
+});
+
+test('json output', () => {
+  const filename1 = getFixturePath('file1.json');
+  const filename2 = getFixturePath('file2.json');
+  expect(getDiff(filename1, filename2, 'json')).toEqual(resultJSON);
 });
