9597d710b69ee83e3b621d58c90c2d1c6756ccee
nana park <obsidinan@gmail.com>
2023-12-30 00:48:21 +0300

    fix compare func

--- src/compare.js
+++ src/compare.js
@@ -1,45 +1,28 @@
 import _ from 'lodash';
 
 const compare = (data1, data2) => {
-  const keys1 = _.keys(data1);
-  const keys2 = _.keys(data2);
-
-  const sortedUnionKeys = _.sortBy(_.union(keys1, keys2));
+  const sortedUnionKeys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
 
   const diffArray = sortedUnionKeys.map((key) => {
     if (!_.has(data1, key)) {
-      return {
-        type: 'added',
-        key,
-        value: data2[key],
-      };
+      return { type: 'added', key, value: data2[key] };
     }
     if (!_.has(data2, key)) {
-      return {
-        type: 'removed',
-        key,
-        value: data1[key],
-      };
+      return { type: 'deleted', key, value: data1[key] };
     }
     if (data1[key] === data2[key]) {
-      return {
-        type: 'unchanged',
-        key,
-        value: data1[key],
-      };
+      return { type: 'unchanged', key, value: data1[key] };
     }
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      return {
-        key,
-        type: 'hasChildren',
-        children: compare(data1[key], data2[key]),
-      };
+      return { key, type: 'nested', children: compare(data1[key], data2[key]) };
     }
     return {
       type: 'changed',
       key,
-      valueRemoved: data1[key],
-      valueAdded: data2[key],
+      value: {
+        valueRemoved: data1[key],
+        valueAdded: data2[key],
+      },
     };
   });
 
