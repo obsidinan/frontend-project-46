eda327fcb79348585b14ecfb753ef6fc5a18eeee
nana park <obsidinan@gmail.com>
2023-12-30 00:25:05 +0300

    fix bugs

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,9 @@
 import plain from './plain.js';
 import stylish from './stylish.js';
-import json from './json.js';
 
 const formatters = {
   plain,
   stylish,
-  json,
 };
 
 export default (data, format) => formatters[format](data);
